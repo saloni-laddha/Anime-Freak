bc98352374f787b1d3b6d7f9c6534bd4343f0016
Saloni Laddha <saloniladdha8@gmail.com>
2018-09-08 01:17:23 +0530

    server part complete

--- bot.py
+++ bot.py
@@ -4,12 +4,14 @@ import random
 
 
 def reply(sentence):
-	index = int(random.randint(0, 99))
+	index = int(random.randint(0, 49))
 	print(index)
 	count1 = 0
 	with open('out.csv', 'r') as file:
 		for line in file:
 			if count1 == index:
+				data = [x.strip() for x in line.split(',')]
+				line = str(data[0]) + ' with IMDb of ' + str(data[1]) + '.'
 				print(line)
 				return (line)
 			count1 += 1
